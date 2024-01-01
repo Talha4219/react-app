696e705c7284605d1209d5b441be0fc85ff572ff
Talha4219 <139109096+Talha4219@users.noreply.github.com>
2024-01-01 19:45:33 +0500

    Update Articles.jsx

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -35,7 +35,7 @@ const Articles = () => {
         ) : isError ? (
           <ErrorMessage message="Couldn't fetch the posts data" />
         ) : (
-          data?.data.map((post) => (
+          data?.data && data.map((post) => (
             <ArticleCard
               key={post._id}
               post={post}
